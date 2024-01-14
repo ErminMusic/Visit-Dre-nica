9fad8ca28571ae8eb7495661b5ce5be8c597c024
Ermin Music <emusic444@gmail.com>
2024-01-14 13:19:36 +0100

    Strip Responsive

--- src/components/Strip.jsx
+++ src/components/Strip.jsx
@@ -49,7 +49,6 @@ export default Strip
 
 const Wrap = styled.div`
   width: 100%;
-  height: 220px;
   background-color: #F8F5DF;
   display: flex;
   justify-content: center;
@@ -61,6 +60,10 @@ const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+
+  @media (max-width: 850px) {
+    width: 100%;
+  } 
 `
 
 const Holder = styled.div`
@@ -68,6 +71,10 @@ const Holder = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  @media (max-width: 520px) {
+    margin: 16px;
+  }
 `
 
 const Wrapper = styled.div`
@@ -75,12 +82,25 @@ const Wrapper = styled.div`
   justify-content: space-evenly;
   align-items: start;
   width: 100%;
+
+  @media (max-width: 700px) {
+    justify-content: space-around;
+  }
+
+  @media (max-width: 520px) {
+    flex-direction: column;
+    align-items: center;
+  }
 `
 
 const IconHolder = styled.img`
   width: 80px;
   border-radius: 10px 10px 10px 0;
   margin: 0 0 12px 0;
+
+  @media (max-width: 520px) {
+    width: 100px;
+  }
 `
 
 const TextHolder = styled.div`
@@ -88,4 +108,8 @@ const TextHolder = styled.div`
   color: #525252;
   font-weight: bold;
   text-align: center;
+  
+  @media (max-width: 520px) {
+    font-size: 18px;
+  }
 `
