a99a1ca3346ee2410ec4a0f0800144113879ae1a
Ermin Music <emusic444@gmail.com>
2024-01-16 12:16:37 +0100

    Performanse Fixes

--- src/components/main-components/Home.jsx
+++ src/components/main-components/Home.jsx
@@ -1,8 +1,10 @@
-import AboutDreznica from "../sections/AboutDreznica"
-import Hero from "../sections/Hero"
-import RoomEquipment from "../sections/RoomEquipment"
-import Rooms from "../sections/Rooms"
-import Strip from "../sections/Strip"
+import React from 'react';
+
+const Hero = React.lazy(() => import('../sections/Hero'));
+const AboutDreznica = React.lazy(() => import('../sections/AboutDreznica'));
+const RoomEquipment = React.lazy(() => import('../sections/RoomEquipment'));
+const Rooms = React.lazy(() => import('../sections/Rooms'));
+const Strip = React.lazy(() => import('../sections/Strip'));
 
 function Home() {
   return (
