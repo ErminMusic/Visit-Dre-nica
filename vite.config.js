74ced0ecc66b6c45948c6536e0646ecd429f0603
Ermin Music <emusic444@gmail.com>
2024-02-14 18:44:51 +0100

    Analytics SetUp 5

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import partytownVite from "@builder.io/partytown/utils";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        react(),
-        partytownVite({
-            dest: "partytown",
-        }),
-    ],
+    plugins: [react()],
 });
