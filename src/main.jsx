e22754a24579dbc283a1f42b8a0dcebc67f0dca6
Ermin Music <emusic444@gmail.com>
2024-02-14 16:32:30 +0100

    Analytics SetUp 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import ReactGA from 'react-ga4';
+// import ReactGA from 'react-ga4';
 import './index.css'
 
-ReactGA.initialize("G-6TCDM52R17")
-ReactGA.send({
-  hitType: "pageview",
-  page: window.location.pathname,
-});
+// ReactGA.initialize("G-6TCDM52R17")
+// ReactGA.send({
+//   hitType: "pageview",
+//   page: window.location.pathname,
+// });
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
