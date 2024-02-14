ca3f10a110078b22ceefb01b1bd4478aef734d09
Ermin Music <emusic444@gmail.com>
2024-02-14 16:46:27 +0100

    Analytics SetUp 3

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Header from "./components/sections/Header";
 import { lazy, Suspense } from "react";
 const Footer = lazy(() => import("./components/sections/Footer"));
 import Home from "./components/main-components/Home";
+import { useEffect } from "react";
+import ReactGA from "react-ga4";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 const ActivitiesDetail = lazy(() => import("./components/main-components/ActivitiesDetail"));
 const NearbyAttractions = lazy(() => import("./components/main-components/NearbyAttractions"));
@@ -12,6 +14,20 @@ const Contact = lazy(() => import("./components/main-components/Contact"));
 const HowToBook = lazy(() => import("./components/main-components/HowToBook"));
 
 function App() {
+    ReactGA.initialize("G-6TCDM52R17");
+    useEffect(() => {
+        const script = document.createElement("script");
+        script.async = true;
+        script.src = `https://www.googletagmanager.com/gtag/js?id=G-6TCDM52R17`; // Replace with your tracking ID
+        document.head.appendChild(script);
+
+        window.dataLayer = window.dataLayer || [];
+        window.dataLayer.push({
+            gtm: new Date().getTime(),
+            event: "pageview",
+        });
+        return () => document.head.removeChild(script);
+    }, []);
     return (
         <Container>
             <Router>
